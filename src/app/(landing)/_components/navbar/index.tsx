2c337776024084ee22f9515777cf3abe4bd1dedf
Sayan Das <sayandas61668@gmail.com>
2024-12-18 13:23:09 +0530

    finally deploy 3 navbar fixed 2

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -7,6 +7,7 @@ import { RiMenu5Line } from "react-icons/ri";
 import { auth } from "../../../../../auth";
 import Menu from "./menu";
 import UserBtn from "./user-btn";
+import HamLogo from "@/icons/ham-logo";
 
 const LandingPageNavbar = async () => {
   const session = await auth();
@@ -21,32 +22,15 @@ const LandingPageNavbar = async () => {
               <Menu orientation="desktop" />
               <div className="flex gap-2 w-[100px] justify-end ">
                 {session?.user?.email ? (
-                  <div className="md:flex gap-4 hidden">
-                    <Link href="/agency">
-                      <Button
-                        variant="outline"
-                        className="bg-themeBlack element rounded-2xl h-9 flex gap-2 border-themeGray elemrnt hover:bg-themeGray"
-                      >
-                        <Compass />
-                        Create
-                      </Button>
-                    </Link>
-                    <UserBtn
-                      imageUrl={session?.user?.image || "/user.png"}
-                      username={session?.user?.name || ""}
-                      email={session?.user?.email || ""}
+                  <Link href="/agency">
+                    <Button
+                      variant="outline"
+                      className="bg-themeBlack element rounded-2xl h-9 flex gap-2 border-themeGray elemrnt hover:bg-themeGray"
                     >
-                      <div className="w-9 h-9 rounded-full overflow-hidden">
-                        <Image
-                          alt="profile-image"
-                          src={session?.user?.image || "/user.png"}
-                          className="w-full h-full"
-                          width={100}
-                          height={100}
-                        />
-                      </div>
-                    </UserBtn>
-                  </div>
+                      <Compass />
+                      Create
+                    </Button>
+                  </Link>
                 ) : (
                   <Link href="/agency/sign-in">
                     <Button
@@ -58,6 +42,23 @@ const LandingPageNavbar = async () => {
                     </Button>
                   </Link>
                 )}
+                <div className="md:flex gap-4 hidden">
+                  <UserBtn
+                    imageUrl={session?.user?.image || "/user.png"}
+                    username={session?.user?.name || ""}
+                    email={session?.user?.email || ""}
+                  >
+                    <div className="w-9 h-9 rounded-full overflow-hidden">
+                      <Image
+                        alt="profile-image"
+                        src={session?.user?.image || "/user.png"}
+                        className="w-full h-full"
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  </UserBtn>
+                </div>
 
                 <GlassSheet
                   triggerClass="lg:hidden"
@@ -66,7 +67,7 @@ const LandingPageNavbar = async () => {
                       variant="ghost"
                       className="hover:bg-transparent px-0"
                     >
-                      <RiMenu5Line size={25} />
+                      <HamLogo />
                     </Button>
                   }
                 >
