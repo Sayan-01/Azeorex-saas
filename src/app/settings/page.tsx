4c17359da08e645f8595cc5327e464694d1e96c0
Sayan Das <sayandas61668@gmail.com>
2025-01-15 20:29:38 +0530

    now we make components in editor

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Try1 from "./Try1";
-import DragDropCopy from "./Try3";
 
 const page = () => {
-  return <DragDropCopy />;
+  return <Try1/>;
 };
 
 export default page;
